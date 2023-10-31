d0cd5bfa03930b43e37931780ee1c5330d016752
Astrid GAULTIER <asgaulti@bess-f2r5s4.clusters.42paris.fr>
2023-10-31 17:40:10 +0100

    multimap8_bis notes et nettoyage main_mmap

--- main_mmap.cpp
+++ main_mmap.cpp
@@ -22,42 +22,7 @@ struct classcomp_mmap
 
 int main_mmap ()
 {
-//   multimap<char,int> first;
-
-//   first.insert(ft::pair<char,int>('a',10));
-//   first.insert(ft::pair<char,int>('b',15));
-//   first.insert(ft::pair<char,int>('b',20));
-//   first.insert(ft::pair<char,int>('c',25));
-
-//   multimap<char,int> second (first.begin(),first.end());
-
-//   multimap<char,int> third (second);
-
-//   multimap<char,int,classcomp> fourth;                 // class as Compare
-
-//   bool(*fn_pt)(char,char) = fncomp;
-//   multimap<char,int,bool(*)(char,char)> fifth (fn_pt); // function pointer as comp
-
-//     // initialize container
-//     multimap<int, int> mp;
- 
-//     // insert elements in random order
-//     mp.insert(ft::pair<int,int>( 2, 30 ));
-//     mp.insert(ft::pair<int,int>( 1, 40 ));
-//     mp.insert(ft::pair<int,int>( 3, 60 ));
-//     mp.insert(ft::pair<int,int>( 2, 20 ));
-//     mp.insert(ft::pair<int,int>( 5, 50 ));
- 
-// 	multimap<int, int>::iterator itr = mp.begin();
-//     // prints the elements
-//     std::cout << "KEY\tELEMENT\n";
-//     for (; itr != mp.end(); ++itr) 
-// 	{
-//         std::cout << itr->first << '\t' << itr->second << '\n';
-//     }
-//     return 0;
-
-	multimap<char,int> mymultimap;
+ 	multimap<char,int> mymultimap;
 	multimap<char,int>::iterator it;
 
 	// first insert function version (single parameter):
@@ -100,13 +65,7 @@ int main_mmap ()
 		std::cout << (*it).first << " => " << (*it).second << '\n';
 	// anothermultimap.get_rbt().print_prefix(anothermultimap.get_rbt().getRoot(), 0);
 
-
-
 	return 0;
 }
 
-// Pbs a regler :
-// insert du c, 300 ne se fait pas, pk? si on enleve le c, 30, plus de souci (donc pb avec le max efficiency inserting)
-// leak du a un pb de clear du rbt?
-// insert 3 a regler pour compiler > ok mais difference au resultat dans l'ordre ou sont donnes les deux z 
-// > imprimer le rbt pour voir > pb commentappeler la methode?(getter le rbt (mais comment?) puis faire print_prefix(???, 0)
+A faire : testeur pour differentes fcts 
